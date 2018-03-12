ad83b3a882eb70fb1a782ca8cc2effae73f18951
Mike Innes <mike.j.innes@gmail.com>
2018-03-12 01:48:11 +0000

    import convenience

--- src/WebAssembly.jl
+++ src/WebAssembly.jl
@@ -4,4 +4,14 @@ include("wasm.jl")
 include("passes.jl")
 include("looper.jl")
 
+module Instructions
+
+using ..WebAssembly: Instruction, Const, Nop, Local, SetLocal, Op, Select,
+  Block, If, Loop, Branch, Return, Label, Goto
+
+export Instruction, Const, Nop, Local, SetLocal, Op, Select,
+  Block, If, Loop, Branch, Return, Label, Goto
+
+end
+
 end # module
