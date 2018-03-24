04b16379cf5e657305516791922336269bdab7cf
Tom Short <tshort.rlists@gmail.com>
2018-03-24 14:30:06 -0400

    WIP: add import and export sections

--- src/wasm.jl
+++ src/wasm.jl
@@ -79,13 +79,28 @@ struct Unreachable <: Instruction end
 const unreachable = Unreachable()
 
 struct Func
+  name::Symbol
   params::Vector{WType}
   returns::Vector{WType}
   locals::Vector{WType}
   body::Block
 end
 
+struct Import
+  mod::Symbol
+  name::Symbol
+  typ::Symbol   # :func, :table, :memory, :global
+  params::Vector{WType}
+end
+
+struct Export
+  name::Symbol
+  typ::Symbol   # :func, :table, :memory, :global
+end
+
 struct Module
+  imports::Vector{Import}
+  exports::Vector{Export}
   funcs::Vector{Func}
 end
 
@@ -139,6 +154,22 @@ end
 
 Base.show(io::IO, i::Union{Block,Loop,If}) = printwasm(io, i, 0)
 
+function printwasm(io, x::Export, level)
+  print(io, "\n", "  "^(level))
+  print(io, "(export \"$(x.name)\" ($(x.typ) \$$(x.name)))")
+end
+
+function printwasm(io, x::Import, level)
+  print(io, "\n", "  "^(level))
+  print(io, "(import \"$(x.mod)\" \"$(x.name)\" ($(x.typ) \$$(x.name)")
+  if x.typ == :func && length(x.params) > 0
+    print(io, " (param")
+    foreach(p -> print(io, " $p"), x.params)
+    print(io, ")")
+  end
+  print(io, "))")
+end
+
 function Base.show(io::IO, f::Func)
   print(io, "(func")
   foreach(p -> print(io, " (param $p)"), f.params)
@@ -148,5 +179,26 @@ function Base.show(io::IO, f::Func)
     foreach(p -> print(io, " (local $p)"), f.locals)
   end
   printwasm_(io, f.body.body, 1)
+  print(io, ")")
+end
+
+function printwasm(io::IO, f::Func, level)
+  print(io, "(func")
+  printwasm_(io, f.body.body, 1)
+  foreach(p -> printwasm_(io, " (param $p)", level + 1), f.params)
+  foreach(p -> printwasm_(io, " (result $p)", level + 1), f.returns)
+  if !isempty(f.locals)
+    print(io, "\n ")
+    foreach(p -> printwasm_(io, " (local $p)", level + 1), f.locals)
+  end
+  printwasm_(io, f.body.body, level + 1)
+  print(io, ")")
+end
+
+function Base.show(io::IO, m::Module)
+  print(io, "(module")
+  foreach(p -> printwasm(io, p, 1), m.imports)
+  foreach(p -> printwasm(io, p, 1), m.exports)
+  foreach(p -> printwasm(io, p, 1), m.funcs)
   print(io, ")")
 end
