f1ec66c3a223179e28ade62527c32e638217bbd1
Short <tshort.rlists@gmail.com>
2018-04-04 19:28:27 -0400

    Add Call and update Export

--- src/wasm.jl
+++ src/wasm.jl
@@ -70,6 +70,10 @@ struct Branch <: Instruction
   level::Int
 end
 
+struct Call <: Instruction
+  name::Symbol
+end
+
 Branch(l::Integer) = Branch(false, l)
 
 struct Return <: Instruction end
@@ -95,6 +99,7 @@ end
 
 struct Export
   name::Symbol
+  internalname::Symbol
   typ::Symbol   # :func, :table, :memory, :global
 end
 
@@ -113,6 +118,7 @@ Base.show(io::IO, i::Const)    = print(io, i.typ, ".const ", value(i))
 Base.show(io::IO, i::Local)    = print(io, "get_local ", i.id)
 Base.show(io::IO, i::SetLocal) = print(io, i.tee ? "tee_local" : "set_local ", i.id)
 Base.show(io::IO, i::Op)       = print(io, i.typ, ".", i.name)
+Base.show(io::IO, i::Call)     = print(io, "call \$", i.name)
 Base.show(io::IO, i::Convert)  = print(io, i.to, ".", i.name, "/", i.from)
 Base.show(io::IO, i::Select)   = print(io, "select")
 Base.show(io::IO, i::Branch)   = print(io, i.cond ? "br_if " : "br ", i.level)
@@ -158,7 +164,7 @@ Base.show(io::IO, i::Union{Block,Loop,If}) = printwasm(io, i, 0)
 
 function printwasm(io, x::Export, level)
   print(io, "\n", "  "^(level))
-  print(io, "(export \"$(x.name)\" ($(x.typ) \$$(x.name)))")
+  print(io, "(export \"$(x.name)\" ($(x.typ) \$$(x.internalname)))")
 end
 
 function printwasm(io, x::Import, level)
