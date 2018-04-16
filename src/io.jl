303d51bda159704570a687d7e5bfc7a287ada2a9
Short <tshort.rlists@gmail.com>
2018-04-16 19:28:59 -0400

    Docstrings; bugfix

--- src/io.jl
+++ src/io.jl
@@ -1,10 +1,21 @@
+export write_wasm, write_wast
 
+"""
+    write_wast(filename, m::Module)
+
+Write the WebAssembly module `m` to WebAssembly text format in `filename`.
+"""
 function write_wast(filename, m::Module)
   open(filename, "w") do f
-    show(io, m)       
+    show(f, m)       
   end
 end
 
+"""
+    write_wast(filename, m::Module)
+
+Write the WebAssembly module `m` to WebAssembly binary format in `filename`.
+"""
 function write_wasm(filename, m::Module)
   wastname = tempname() * ".wast"
   write_wast(wastname, m)
