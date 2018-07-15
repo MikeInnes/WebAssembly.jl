41b6dc552e97fc445d95ca6de198883d48d354cb
Sean Innes <sean.r.innes@googlemail.com>
2018-07-16 00:30:25 +0100

    Remove comment about loops and clean code some

--- src/passes.jl
+++ src/passes.jl
@@ -93,15 +93,21 @@ using LightGraphs
 # alive is essentially a set of currently used registers, but with the id of the
 # value stored.
 
-function liveness(code, rig=SimpleGraph(), lines=Dict{Int, Vector{Ref}}(), alive=Dict{Int, Int}(), prev_alive=[], first_pass_loop::Bool=false, skippedsets=Vector())
+function liveness(func::Func)
+  rig = SimpleGraph()
+  liveness_(func.body.body, rig)
+  return rig
+end
+
+function liveness_(code, rig=SimpleGraph(), lines=Dict{Int, Vector{Ref}}(), alive=Dict{Int, Int}(), branches=[], loop::Bool=false, skippedsets=Vector())
   for i in length(code):-1:1
     x = code[i]
     if x isa Local
-      @show x, alive
+      # @show x, alive
       id = get!(alive, x.id) do
-        id = if (@show first_pass_loop) && haskey(prev_alive[end], x.id)
-          println("So this happens, $x")
-          prev_alive[end][x.id]
+        id = if loop && haskey(branches[end][1], x.id)
+          # println("So this happens, $x")
+          branches[end][x.id]
         else
           add_vertex!(rig)
           nv(rig)
@@ -113,10 +119,10 @@ function liveness(code, rig=SimpleGraph(), lines=Dict{Int, Vector{Ref}}(), alive
         return id
       end
       push!(lines, id => push!(lines[id], Ref(code, i)))
-    @show alive
+    # @show alive
     elseif x isa SetLocal
-      @show x
-      if x.id in keys(alive)
+      # @show x
+      if haskey(alive, x.id)
         id = alive[x.id]
         delete!(alive, x.id)
         push!(lines, id => push!(lines[id], Ref(code, i)))
@@ -125,92 +131,66 @@ function liveness(code, rig=SimpleGraph(), lines=Dict{Int, Vector{Ref}}(), alive
         # TODO: Add support for drop / backprop to remove need for Drop.
         push!(skippedsets, Ref(code, i))
       end
-    elseif x isa Block
-      push!(prev_alive, copy(alive))
-      alive = liveness(code[i].body, rig, lines, alive, prev_alive, first_pass_loop, skippedsets)
-      pop!(prev_alive)
+    elseif x isa Block || x isa Loop
+      push!(branches, (copy(alive), x isa Loop))
+      alive = liveness_(code[i].body, rig, lines, alive, branches, loop, skippedsets)
+      pop!(branches)
     elseif x isa If
       a_t = copy(alive)
       a_f = copy(alive)
-      push!(prev_alive, copy(alive))
-      a_t = liveness(code[i].t, rig, lines, a_t, prev_alive, first_pass_loop, skippedsets)
-      a_f = liveness(code[i].f, rig, lines, a_f, prev_alive, first_pass_loop, skippedsets)
-      pop!(prev_alive)
+      push!(branches, (copy(alive), false))
+      a_t = liveness_(code[i].t, rig, lines, a_t, branches, loop, skippedsets)
+      a_f = liveness_(code[i].f, rig, lines, a_f, branches, loop, skippedsets)
+      pop!(branches)
       alive = merge(a_t, a_f)
     elseif x isa Branch
       # At a branch need to revert the state of alive to what it was just after
       # the block. (I.e. just before in terms of calculation order.)
-      if x.cond
-        a_t = copy(prev_alive[end-x.level])
-        a_f = copy(alive)
-        a_t = liveness(code[1:i-1], rig, lines, a_t, prev_alive, first_pass_loop, skippedsets)
-        a_f = liveness(code[1:i-1], rig, lines, a_f, prev_alive, first_pass_loop, skippedsets)
-        alive = merge(a_t, a_f)
-        break;
+
+
+      br = branches[end-x.level]
+      a_b = copy(br[1])
+      if !br[2] # If branching to a loop.
+        a_b = liveness_(code[1:i-1], rig, lines, a_b, branches, loop, skippedsets)
       else
-        # error("no non conditional branches")
-        a_b = copy(prev_alive[end-x.level])
-        a_b = liveness(code[1:i-1], rig, lines, a_b, prev_alive, first_pass_loop, skippedsets)
-        alive = a_b
-        break
-      end
-    elseif x isa Loop
-      push!(prev_alive, copy(alive))
-      s_sets = Vector();
-      alive = liveness(code[i].body, rig, lines, alive, prev_alive, true, s_sets)
-      # liveness(s_sets, rig, lines, alive, prev_alive)
-      @show (1, alive)
-      # s_sets will be in reverse order as required.
-      for s in s_sets
-        set = getindex(s)
-        if set.id in keys(alive)
-          println("here!!")
-          # id = alive[getindex(s).id]
-          # delete!(alive, getindex(s).id)
-          # @show haskey(lines, id)
-          # push!(lines, set.id => s)
-          push!(lines, set.id => push!(lines[set.id], s))
-        else
-          println("here!, $(set)")
-          push!(skippedsets, s)
+        s_sets = Vector();
+        a_b = liveness_(code[1:i-1], rig, lines, a_b, branches, true, s_sets)
+        # liveness(s_sets, rig, lines, alive, branches)
+        # @show (1, alive)
+        # s_sets will be in reverse order as required.
+        for s in s_sets
+          set = getindex(s)
+          if haskey(a_b, set.id)
+            push!(lines, set.id => push!(lines[set.id], s))
+          else
+            push!(skippedsets, s)
+          end
         end
       end
-
-
-
-      pop!(prev_alive)
+      if x.cond
+        a_f = copy(alive)
+        a_f = liveness_(code[1:i-1], rig, lines, a_f, branches, loop, skippedsets)
+        merge!(a_b, a_f)
+      end
+      alive = a_b
+      break
     end
   end
 
-  if first_pass_loop
+  if loop
     return alive
   elseif !isempty(skippedsets)
     println("Warning: Setting locals but values aren't used (Can replace with Drop).")
-    @show map(getindex, skippedsets), map(s -> s.i, skippedsets)
+    println((map(getindex, skippedsets), map(s -> s.i, skippedsets)))
   end
   return alive
-
-  # return rig
 end
-
-# TODO: Loops. The problem lies in the fact that the same set_local can be used
-# for what might be 2 different value_ids.
-
-# loop
-#   get_local 1 <- some value id from before the loop, or the value id from the set below.
-#    ....
-#   set_local 1 <- creates a new value id.
-# end
-
-# In all other constructs flow control is just sequential down the program, so
-# there's no chance of this happening. It should be possible to perform a trick
-# to fix it such as reformatting so any get performed before a set is done
-# outside of the loop.
 
 using GraphLayout
 function drawGraph(filename, graph)
   # Output the graph
   am = Matrix(adjacency_matrix(graph))
   loc_x, loc_y = layout_spring_adj(am)
+  # draw_layout_adj(am, loc_x, loc_y, filename=filename, arrowlengthfrac=0)
   draw_layout_adj(am, loc_x, loc_y, filename=filename)
 end
