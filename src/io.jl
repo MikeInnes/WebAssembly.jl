0b1e90e54cebe500303bf6d8566a81a47256493d
Mike J Innes <mike.j.innes@gmail.com>
2019-10-07 22:11:31 +0100

    binary output

--- src/io.jl
+++ src/io.jl
@@ -7,7 +7,7 @@ Write the WebAssembly module `m` to WebAssembly text format in `filename`.
 """
 function write_wast(filename, m::Module)
   open(filename, "w") do f
-    show(f, m)       
+    show(f, m)
   end
 end
 
@@ -16,22 +16,10 @@ end
 
 Write the WebAssembly module `m` to WebAssembly binary format in `filename`.
 """
-function write_wasm(filename, m::Module)
-  wastname = tempname() * ".wast"
-  write_wast(wastname, m)
-  wast2wasm(wastname, filename)
+function binary(m::Module, file)
+  wast = tempname() * ".wast"
+  write_wast(wast, m)
+  wast2wasm(wast, file)
+  rm(wast)
+  return
 end
-
-"""
-    wast2wasm(input, output)
-
-Convert the text WebAssembly file named `input` to the binary WebAssembly file `output`.
-"""
-wast2wasm(input, output) = run(`$(WABT.wast2wasm) $input -o $output`)
-
-"""
-    wasm2wast(input, output)
-
-Convert the binary WebAssembly file named `input` to the text WebAssembly file `output`.
-"""
-wasm2wast(input, output) = run(`$(WABT.wasm2wast) $input -o $output`)
